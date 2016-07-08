a67e035707929078a3030ae535d7e74de8b4290b
Andrew <andrew.mecham@gmail.com>
2016-07-08 17:58:24 -0600

    added button feature, created new file timeago

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     // Tweet input box animations
-    // $('#tweet-controls').hide();
+
+    var userName = "Andrew";
+    var profilePic = "alagoon";
+
+    $('#tweet-controls').hide();
 
     $(".tweet-compose").click(function() {
         $("#tweet-controls").show(function() {
@@ -10,16 +14,17 @@ $(document).ready(function() {
         });
     });
 
-    // // $(".tweet-compose").blur(function() {
-    // //     $("#tweet-controls").hide(function() {
-    // //         $(".tweet-compose").animate({
-    // //             height: "2.5em"
-    // //         });
-    //     });
-    // });
+    $(".tweet-compose").blur(function() {
+        $("#tweet-controls").hide(function() {
+            $(".tweet-compose").animate({
+                height: "2.5em"
+            });
+        });
+    });
 
     $(".tweet-compose").on("keyup", counter);
-// #tweet-submit;
+// steps 1 through 4;
+  tweetText = $('.tweet-compose').val();
     function counter() {
         var newCount = $('.tweet-compose').val().length;
         console.log(newCount);
@@ -36,8 +41,17 @@ $(document).ready(function() {
         if (newCount <= 140) {
           $("#tweet-submit").removeClass("button:disabled").addClass("button");
         }
-
     }
 
+// tweet transfer
+  $('#tweet-submit').on('click', function() {
+    newTweet = $('<div class="tweet"><div class="content"><img class="avatar" src="img/' + profilePic + '.jpg" /><strong class="fullname">' + userName + '</strong><span class="username"> @' + userName + '</span><span class="upperTime"><time class="timeago"> &sdot; ' + jQuery.timeago(new Date()) + '</time></span><p class="tweet-text">' + tweetText + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="stats"><div class="retweets"><p class="num-retweets">30</p><p>RETWEETS</p></div><div class="favorites"><p class="num-favorites">6</p><p>FAVORITES</p></div><div class="users-interact"><div><img src="img/alagoon.jpg" /><img src="img/vklimenko.jpg" /></div></div><div class="time"><time class="timeago">' + jQuery.timeago(new Date()) + '</time></div><div class="reply"><img class="avatar" src="img/alagoon.jpg" /><textarea class="tweet-compose-reply" placeholder="Reply to @' + userName + '" /></textarea></div></div></div>');
+    if (tweetText) {
+        $(newTweet).prependTo('#stream').fadeIn(1000);
+        tweetText = null;
+        $('.tweet-compose').val(null);
+        $('#char-count').text("140");
+    }
+});
 
     });
