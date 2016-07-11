08ad8ed085f33112c7748e2d35225dd8765dac8c
Andrew <andrew.mecham@gmail.com>
2016-07-11 07:46:29 -0600

    updated new tweet window

--- js/animations.js
+++ js/animations.js
@@ -3,13 +3,20 @@ $(document).ready(function() {
 
     jQuery("time.timeago").timeago();
 
-    var userName = "Andrew";
-    var profilePic = "alagoon";
+
     var tweetText;
+    var resetInput = function (){
+           $('.tweet-compose').delay(100).animate({height: '2.5em'}, 'fast');
+           $('#tweet-controls').hide();
+    };
+    resetInput();
+
+
     // Hide tweet buttons
     $('#tweet-controls').hide();
 
     // Reveal tweet button when the input box it clicked
+    function resetText(){
     $(".tweet-compose").click(function() {
         $("#tweet-controls").show(function() {
             $(".tweet-compose").animate({
@@ -25,6 +32,8 @@ $(document).ready(function() {
             });
         });
     });
+}
+resetText();
 
     $(".tweet-compose").on("keyup", counter);
 // steps 1 through 4;
@@ -48,44 +57,27 @@ $(document).ready(function() {
         }
     }
 
-// tweet transfer
-//   $('#tweet-submit').on('click', function() {
-//     newTweet = $('<div class="tweet"><div class="content"><img class="avatar" src="img/' + profilePic + '.jpg" /><strong class="fullname">' + userName + '</strong><span class="username"> @' + userName + '</span><span class="upperTime"><time class="timeago"> &sdot; ' + jQuery.timeago(new Date()) + '</time></span><p class="tweet-text">' + tweetText + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="stats"><div class="retweets"><p class="num-retweets">30</p><p>RETWEETS</p></div><div class="favorites"><p class="num-favorites">6</p><p>FAVORITES</p></div><div class="users-interact"><div><img src="img/alagoon.jpg" /><img src="img/vklimenko.jpg" /></div></div><div class="time"><time class="timeago">' + jQuery.timeago(new Date()) + '</time></div><div class="reply"><img class="avatar" src="img/alagoon.jpg" /><textarea class="tweet-compose-reply" placeholder="Reply to @' + userName + '" /></textarea></div></div></div>');
-//     if (tweetText) {
-//         $(newTweet).prependTo('#stream').fadeIn(1000);
-//         tweetText = null;
-//         $('.tweet-compose').val(null);
-//         $('#char-count').text("140");
-//     }
-// });
 var submitIt = function() {
   $('#tweet-submit').on('click', function(e) {
-    //stores what the user wrote
     var newTweet = $('#tweet-content .tweet-compose').val();
-    //copies first tweet
     $('.tweet:first').clone().prependTo('#stream');
-    //copies the profile pic over
-    $('.avatar:eq(1)').attr('src', 'img/funwatercat.jpg');
-    //copies name over
+    $('.avatar:eq(1)').attr('src', 'img/alagoon.jpg');
     $('.fullname:first').text('Andrew');
     $('.username:first').text('@Andrew');
-    //copies text over
     $('.tweet-text:first').text(newTweet);
     $('.reply .tweet-compose:first').attr("placeholder", "reply to @andrew");
-    //resets to original input
     resetInput();
-    //resets textbox from usert tweet
     e.preventDefault();
     $('#tweet-content .tweet-compose').val('');
     $('#char-count').text('140');
-    //does the enlarge box again when this function is invoked
-    resetCompose();
+    resetText();
     clickTweet();
   });
   };
   submitIt();
 
   clickTweet();
+
 
 
 
